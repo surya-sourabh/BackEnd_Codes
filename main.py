eb544e588a60852e497da94d17417954afe6d171
Sreenath1999 <99864312+Sreenath1999@users.noreply.github.com>
2022-04-01 10:58:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import math
 # head = ["Ranks", "Winning_Amount", "Total_Winners", "Total_Winning Amount"]
 # mydata = [[1, 2, 3, 4], [5, 6, 7, 8]]
 # print(tabulate(mydata, headers=head, tablefmt="grid"))
+import statistics
 
 
 def sumCalc(a):
     tsum = 0.0;
     # print(e_fee, total_winners, max_prize)
-    for i in range(1, total_winners):
+    for i in range(1, total_winners + 1):
         tsum += e_fee + (max_prize - e_fee) / i ** a
     return tsum
 
@@ -24,6 +25,10 @@ def alphSearch(low, high):
     if high >= low:
 
         mid = (high + low) / 2
+
+
+
+
         totSum = sumCalc(mid)
         print(totSum, mid)
 
@@ -48,83 +53,53 @@ max_prize = int(input("Max Winning Price: "))
 prize_total = e_fee * totalplayers
 win_percent = float(input("Win % :  "))
 # 55.42857
+
+s = [25, 100, 300, 500, 1000, 2000, 5000, 10000, 50000, 194000]
+a = 0
+z = 6
 total_winners = math.ceil(win_percent / 100 * float(totalplayers))
+winnings = round(totalplayers * .5542857)
+b2 = winnings - 5
 # prize_topwinners = 0.06 * float(prize_total);
 print("Total_prize: ", prize_total, " Total_Winners: ", total_winners)
 
 # print(sumCalc(0.7))
 alpha, totSum = alphSearch(0, 2)
 
-print("Alpha: ", alpha, " Sum: ", totSum)
-rank={}
-t_prize=0
-for i in range(1, total_winners):
-    print(i, ": ", round(e_fee + (max_prize - e_fee) / i ** alpha))
-    t_prize=t_prize+(e_fee + (max_prize - e_fee) / i ** alpha)
-    key=i
-    value=e_fee + (max_prize - e_fee) / i ** alpha
-    rank[key]=round(value)
-print(rank)
-print(round(t_prize))
-s=[25,100,300,500,1000,2000,5000,10000,50000,194000]
-z=6
-entries=int(input("enter the total entires"))
-a=0
-winners=round(entries*.5542857)
-b2=winners-5
-def maprange(a,b,s):
-    (a1, a2), (b1,b2)=a,b
-    t=round(b1+((s-a1)*(b2-b1)/(a2-a1)))
+print("Alpha: ", alpha, " Sum: ", round(totSum))
 
+
+def maprange(a, b, s):
+    (a1, a2), (b1, b2) = a, b
+    t = round(b1 + ((s - a1) * (b2 - b1) / (a2 - a1)))
     return t
-for i in range(6, winners):
-     if(a<10):
-        t=maprange((6,193995),(6 ,b2),s[a])
-        if t>=z:
-            # for j in m_per:
-            print(z,"maps to",t,"totalWinners=",t-z+1,t-z+1*round(e_fee + (max_prize - e_fee) / i ** alpha))
-        a+=1
-        z=t+1
+
+
+t_prize=0
+for i in range(1, total_winners  ):
+    print(i, ": ",round(e_fee + (max_prize - e_fee) / i ** alpha))
+    t_prize = t_prize + (e_fee + (max_prize - e_fee) / i ** alpha)
+    print(round(t_prize))
+#mean = statistics.mean(e_fee + (max_prize - e_fee)/ i ** alpha)
+#print(mean)
+for i in range(6, winnings):
+        if a < 10:
+          t = maprange((6, 193995), (6, b2), s[a])
+          if t >= z:
+              print(i)
+              print(z, " maps to ", t, "winners=" , t - z + 1 ,"prize",i)
+              sum = (t-z+1)*round(e_fee + (max_prize - e_fee) / i ** alpha)
+              print('prize for players for the above rank',sum)
+
+          a += 1
+          z = t + 1
+#mean = statistics.mean(sum)
+#print(mean)
+#def maprange(a, b, s):
+#    (a1, a2), (b1, b2) = a, b
+ #   return b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
 
 # for s in range(11):
-#    print("%2g maps to %g" % (s, maprange((0, 10), (-1, 0), s)))
+#    print("%2g maps to %g" % (s, maprange((0, 10), (-1, 0), s))
 
-
-#
-# s=[25,100,300,500,1000,2000,5000,10000,50000,194000]
-# m_per=[2.857142857, 4.285714286,5.714285714,2.857142857,2.857142857,2.857142857,4.285714286,4.285714286,22.85714286,41.14285714]
-# a=0
-# z=6
-# entries=int(input("enter the total entires"))
-# t_prize=entries
-# print(t_prize)
-# p1=round(entries*2.857142857)/100
-# p2=round(entries*1.428571429)/100
-# p3=round(entries*0.857142857)/100
-# p4=round(entries*0.571428571)/100
-# p5=round(entries*0.285714286)/100
-# # print(p1)
-# # print(p2)
-# # print(p3)
-# # print(p4)
-# # print(p5)
-# d=p1+p2+p3+p4+p5
-# d_prize=t_prize-d
-# print(d_prize)
-# winning_prize=0
-# winners=round(entries*.5542857)
-#
-# b2=winners-5
-# def maprange(a,b,s):
-#     (a1, a2), (b1,b2)=a,b
-#     t=round(b1+((s-a1)*(b2-b1)/(a2-a1)))
-#
-#     return t
-# for i in range(6, winners):
-#      if(a<10):
-#         t=maprange((6,193995),(6 ,b2),s[a])
-#         if t>=z:
-#             # for j in m_per:
-#             print(z,"maps to",t,"totalWinners=",t-z+1)
-#         a+=1
-#         z=t+1
+#55.42857
